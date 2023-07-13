fcb9ef1ee42f6015d0b3cf3ceca75f87630833a0
Diogo Otoni <diogootoni@hotmail.com>
2023-07-13 10:12:50 -0300

    Style - adicao do readme

---
+++ README.md
@@ -0,0 +1,22 @@
+# LOJA com a API do mercado livre
+
+Foram utilizadas as seguintes tecnologias:
+- Vite
+```bash
+  npm npm create vite@latest
+  {PROJECT-NAME}
+  react
+  Javascript + SWC
+
+  cd {PROJECT-NAME}
+
+  npm install
+  npm run dev
+```
+- ReactJS
+
+- AXIOS
+```bash
+npm install axios
+
+```
--- src/pages/Cart/index.jsx
+++ src/pages/Cart/index.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import { Link } from 'react-router-dom';
 import {setItem, getItem} from '../../services/LocalStorageFuncs'
-
+import '../../styles/store.css'
 import {BsFillCartDashFill} from 'react-icons/bs'
 
 
@@ -20,22 +20,24 @@ export default function Cart() {
     return (
         <div>
             <h1>CART</h1>
-            <Link to='/store'>Voltar para as compras</Link>
 
-            <h3>{`SubTotal: R$ ${subTotal}`}</h3>
-            <div>
-                {
-                    data.map((e) => (
-                        <li key={e.id}>
-                                <h4>{e.title}</h4>
-                                <img src={e.thumbnail} alt="" />
-                                <h5>{`R$ ${e.price}`}  </h5>
-                                <button onClick={() => removeItem(e)}>
-                                    <BsFillCartDashFill />
-                                </button>
-                        </li>
-                    ))
-                }
+                    <Link to='/store' className='back-store'>Voltar para as compras</Link>
+            <div className="lista-products">
+                <ul className="list-itens">
+                    {
+                        data.map((e) => (
+                            <li className="product-item" key={e.id}>
+                                    <h4>{e.title}</h4>
+                                    <img src={e.thumbnail} alt="" />
+                                    <h5>{`R$ ${e.price}`}  </h5>
+                                    <button onClick={() => removeItem(e)}>
+                                        <BsFillCartDashFill />
+                                    </button>
+                            </li>
+                        ))
+                    }
+                </ul>
+                <h3 className='title-sub-total'>{`SubTotal: R$ ${subTotal}`}</h3>
             </div>
         </div>
     )
